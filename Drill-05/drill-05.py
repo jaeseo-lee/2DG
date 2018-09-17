71822c2030ca214f8ced8135bec200e93edf8077
jaeseo-lee <dlwotj0914@gmail.com>
2018-09-17 16:27:14 +0900

    rectangle 함수 완성

--- Drill-05/drill-05.py
+++ Drill-05/drill-05.py
@@ -47,10 +47,10 @@ def move_from_left_to_center():
         delay(0.01)
 
 def make_rectangle():
-    # move_from_center_to_right()
-    # move_up()
-    # move_left()
-    # move_down()
+    move_from_center_to_right()
+    move_up()
+    move_left()
+    move_down()
     move_from_left_to_center()
 
 def make_circle():
