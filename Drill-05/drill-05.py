c0017336e4aefcc3d0650ed10fbb2e43bac0e7f6
jaeseo-lee <dlwotj0914@gmail.com>
2018-09-17 21:43:35 +0900

    일곱번째 좌표에서 여덟번째 좌표로 이동
    일곱번째 좌표에서 여덟번째 좌표로 이동

--- Drill-05/drill-05.py
+++ Drill-05/drill-05.py
@@ -128,7 +128,22 @@ def goto_x_y_7():
         delay(0.02)
         get_events()
 def goto_x_y_8():
-    pass
+    x1, x2 = 510, 692
+    y1, y2 = 92, 518
+    x_change = x2 - x1
+    y_change = y2 - y1
+    m = y_change / x_change
+    frame = 0
+    while (x1 < x2):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x1, y1)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x1 += 5
+        y1 += 5 * m
+        delay(0.02)
+        get_events()
 def goto_x_y_9():
     pass
 def goto_x_y_10():
@@ -141,7 +156,7 @@ while True:
     #goto_x_y_4()
     #goto_x_y_5()
     #goto_x_y_6()
-    goto_x_y_7()
+    #goto_x_y_7()
     goto_x_y_8()
     goto_x_y_9()
     goto_x_y_10()
