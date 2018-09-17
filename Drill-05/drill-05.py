cd1990f63ad899f2bf2289dddde73de7f3cc18d4
jaeseo-lee <dlwotj0914@gmail.com>
2018-09-17 16:21:26 +0900

    오른쪽끝아래에서 위로 올리기

--- Drill-05/drill-05.py
+++ Drill-05/drill-05.py
@@ -14,7 +14,13 @@ def move_from_center_to_right():
         x += 2
         delay(0.01)
 def move_up():
-    pass
+    x, y = 800 - 25, 90
+    while y < 600 - 50:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        y += 2
+        delay(0.01)
 def move_left():
     pass
 def move_down():
@@ -23,7 +29,7 @@ def move_from_left_to_center():
     pass
 
 def make_rectangle():
-    move_from_center_to_right()
+    # move_from_center_to_right()
     move_up()
     move_left()
     move_down()
