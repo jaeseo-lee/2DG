fc7a48a2abef719f4a5b02b4cd8118140229ebcf
jaeseo-lee <dlwotj0914@gmail.com>
2018-09-17 16:58:01 +0900

    전체적인 뻐대 완성
    뼈대 완성했습니다.

--- Drill-05/drill-05.py
+++ Drill-05/drill-05.py
@@ -3,72 +3,38 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
-def move_from_center_to_right():
-    x, y = 800 // 2, 90
-    while x < 800 - 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
-def move_up():
-    x, y = 800 - 25, 90
-    while y < 600 - 50:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y += 2
-        delay(0.01)
-def move_left():
-    x, y = 800 - 25, 600 - 50
-    while x > 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x -= 2
-        delay(0.01)
-def move_down():
-    x, y = 25, 600 - 50
-    while y > 90:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y -= 2
-        delay(0.01)
-def move_from_left_to_center():
-    x, y = 25, 90
-    while x < 800 // 2:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
+def goto_x_y_1():
+    pass
+def goto_x_y_2():
+    pass
+def goto_x_y_3():
+    pass
+def goto_x_y_4():
+    pass
+def goto_x_y_5():
+    pass
+def goto_x_y_6():
+    pass
+def goto_x_y_7():
+    pass
+def goto_x_y_8():
+    pass
+def goto_x_y_9():
+    pass
+def goto_x_y_10():
+    pass
 
-def make_rectangle():
-    move_from_center_to_right()
-    move_up()
-    move_left()
-    move_down()
-    move_from_left_to_center()
-
-import math
-
-def make_circle():
-    cx, cy, r = 800 // 2, 600 // 2, (600 - 180) // 2
-    degree = -90
-    while degree < 270:
-        radian = math.radians(degree)
-        x = cx + r * math.cos(radian)
-        y = cy + r * math.sin(radian)
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        degree += 1
-        delay(0.01)
 while True:
-    make_rectangle()
-    make_circle()
-
+    goto_x_y_1()
+    goto_x_y_2()
+    goto_x_y_3()
+    goto_x_y_4()
+    goto_x_y_5()
+    goto_x_y_6()
+    goto_x_y_7()
+    goto_x_y_8()
+    goto_x_y_9()
+    goto_x_y_10()
 close_canvas()
