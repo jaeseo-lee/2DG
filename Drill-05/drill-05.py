be28e2d09ef4d08d252b260de11f20ad8b3e04da
jaeseo-lee <dlwotj0914@gmail.com>
2018-09-17 21:20:47 +0900

    왼쪽하단에서 첫번째 좌표로 이동(수정)
    25,90에서 시작해서 첫번째 좌표로 이동했습니다. (시작방향이 잘못되어 다시 수정했습니다.)

--- Drill-05/drill-05.py
+++ Drill-05/drill-05.py
@@ -3,26 +3,26 @@ import os
 
 os.getcwd()
 os.chdir('C:\\2DGP\\2018-2DGP\\Labs\\Lecture04')
-os.getcwd()
 
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-
-
 def goto_x_y_1():
-    x = 800 // 2
-    y = 600 // 2
+    x1, x2 = 25, 203
+    y1, y2 = 90, 535
+    x_change = x2 - x1
+    y_change = y2 - y1
+    m = y_change // x_change
     frame = 0
-    while (x >= 203 & y <= 535):
+    while (x1 < x2):
         clear_canvas_now()
         grass.draw_now(400, 30)
-        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        character.clip_draw(frame * 100, 100, 100, 100, x1, y1)
         update_canvas()
         frame = (frame + 1) % 8
-        x = x - 1
-        y = y + 1
+        x1 += 5
+        y1 += 5 * m
         delay(0.02)
         get_events()
 def goto_x_y_2():
