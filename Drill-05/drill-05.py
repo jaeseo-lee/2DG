57e92edbebe6271e7dd224586c37c96e4a679bb5
jaeseo-lee <dlwotj0914@gmail.com>
2018-09-17 19:41:16 +0900

    중점에서 첫번째 좌표로 이동
    400,300 에서 시작해서 첫번째 좌표인
    (203, 535) 로이동시켰습니다.

--- Drill-05/drill-05.py
+++ Drill-05/drill-05.py
@@ -1,12 +1,30 @@
 from pico2d import *
+import os
+
+os.getcwd()
+os.chdir('C:\\2DGP\\2018-2DGP\\Labs\\Lecture04')
+os.getcwd()
 
 open_canvas()
-
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
+
+
 def goto_x_y_1():
-    pass
+    x = 800 // 2
+    y = 600 // 2
+    frame = 0
+    while (x >= 203 & y <= 535):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x = x - 1
+        y = y + 1
+        delay(0.02)
+        get_events()
 def goto_x_y_2():
     pass
 def goto_x_y_3():
