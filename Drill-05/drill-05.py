9f9342b7631b770db43426f200e6244cd3b9ac21
jaeseo-lee <dlwotj0914@gmail.com>
2018-09-17 21:24:16 +0900

    첫번째 좌표에서 두번째 좌표로 이동
    첫번째 좌표에서 두번째 좌표로 이동

--- Drill-05/drill-05.py
+++ Drill-05/drill-05.py
@@ -26,7 +26,22 @@ def goto_x_y_1():
         delay(0.02)
         get_events()
 def goto_x_y_2():
-    pass
+    x1, x2 = 203, 132
+    y1, y2 = 535, 243
+    x_change = x2 - x1
+    y_change = y2 - y1
+    m = y_change // x_change
+    frame = 0
+    while (x1 > x2):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x1, y1)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x1 -= 5
+        y1 -= 5 * m
+        delay(0.02)
+        get_events()
 def goto_x_y_3():
     pass
 def goto_x_y_4():
@@ -45,7 +60,7 @@ def goto_x_y_10():
     pass
 
 while True:
-    goto_x_y_1()
+    #goto_x_y_1()
     goto_x_y_2()
     goto_x_y_3()
     goto_x_y_4()
