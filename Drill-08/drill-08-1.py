ca63d863c71c859047254d30bbb2f922d81210ba
jaeseo-lee <dlwotj0914@gmail.com>
2018-10-04 16:54:51 +0900

    drill-08-1 완성
    2014182032 이재서
    커밋횟수 총 2회
    https://www.youtube.com/watch?v=J9ayZq3PVSA&feature=share

--- Drill-08/drill-08-1.py
+++ Drill-08/drill-08-1.py
@@ -96,6 +96,10 @@ def draw_curve_4_points(p1, p2, p3, p4):
 
 prepare_turtle_canvas()
 
+points = [(-300, 200), (400, 350), (300, -300), (-200, -200)]
+size = len(points)
 
+while True:
+    draw_curve_4_points(points[0], points[1], points[2], points[3])
 
 turtle.done()
