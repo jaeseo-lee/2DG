65fc041728e0f94c586344820ad1e7b8136365d1
jaeseo-lee <dlwotj0914@gmail.com>
2018-10-04 17:09:30 +0900

    drill-08-2 # draw 7 -> 8
    일곱번째에서 여덟번째로 이동

--- Drill-08/drill-08-2.py
+++ Drill-08/drill-08-2.py
@@ -90,6 +90,16 @@ while True:
     frame = 0
 
     # draw 7 -> 8
+    for i in range(0, 100, 1):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * xPosition[5] + (3 * t ** 3 - 5 * t ** 2 + 2) * xPosition[6] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * xPosition[7] + (t ** 3 - t ** 2) * xPosition[8]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * yPosition[5] + (3 * t ** 3 - 5 * t ** 2 + 2) * yPosition[6] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * yPosition[7] + (t ** 3 - t ** 2) * yPosition[8]) / 2
+        Draw(x, y)
+
+    frame = 0
+
     # draw 8 -> 9
     # draw 9 -> 10
     # draw 10 -> 1
