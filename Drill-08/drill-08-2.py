6618b8fac79220441dd428bce4db70b7a67aea6b
jaeseo-lee <dlwotj0914@gmail.com>
2018-10-04 17:05:38 +0900

    drill-08-2  # draw 3 -> 4
    세번째에서 네번째 좌표로 이동

--- Drill-08/drill-08-2.py
+++ Drill-08/drill-08-2.py
@@ -46,6 +46,16 @@ while True:
     frame = 0
 
     # draw 3 -> 4
+    for i in range(0, 100, 1):
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * xPosition[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * xPosition[2] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * xPosition[3] + (t ** 3 - t ** 2) * xPosition[4]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * yPosition[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * yPosition[2] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * yPosition[3] + (t ** 3 - t ** 2) * yPosition[4]) / 2
+        Draw(x, y)
+
+    frame = 0
+
     # draw 4 -> 5
     # draw 5 -> 6
     # draw 6 -> 7
