75d8475bfb0f47d506910cf43ff6f80e07dbdcf0
jaeseo-lee <dlwotj0914@gmail.com>
2018-10-08 17:32:06 +0900

    drill-09 Ball 클래스 추가
    기본

--- Drill-09/drill-09.py
+++ Drill-09/drill-09.py
@@ -22,7 +22,18 @@ class Boy:
         self.image.clip_draw(self.frame*100, 0, 100, 100, self.x, self.y)
 
 class Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(10, 780), 700
+        self.ball_size = random.randint(0, 1)
+        self.speed = random.randint(5, 20)
 
+        if self.ball_size == 0:
+            self.image = load_image('ball21x21.png')
+        elif self.ball_size == 1:
+            self.image = load_image('ball41x41.png')
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
 
 def handle_events():
     global running
