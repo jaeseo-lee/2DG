e264cfab602f9130ef044989d4612f6cbfc0c21e
jaeseo-lee <dlwotj0914@gmail.com>
2018-11-27 02:20:42 +0900

    2번째 기체 3개로 나눠 쏘기
    2번째 기체 3개로 나눠 쏘기

--- 1107/enemy_bullet2.py
+++ 1107/enemy_bullet2.py
@@ -34,3 +34,70 @@ class Enemy_Bullet2:
 
         if self.y < 20:
             game_world.remove_object(self)
+
+
+class Enemy_Bullet3:
+    image = None
+
+    def __init__(self, x=300, y=300):
+        velocity = 1.5
+        damage = 1
+        if Enemy_Bullet3.image == None:
+            Enemy_Bullet3.image = load_image('EnemyGun2.png')
+        self.x, self.y, self.velocity = x, y, velocity
+
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 12, self.y - 10, self.x + 12, self.y + 10
+
+    def update(self):
+        self.y -= self.velocity
+        self.x += self.velocity / 3
+
+        if main_state.collide(self, main_state.player):
+            game_world.remove_object(self)
+
+
+        #if main_state.player.hp == 0:
+            #game_world.remove_object(main_state.player)
+            #main_state.player.life -= 1
+
+        if self.y < 20:
+            game_world.remove_object(self)
+
+class Enemy_Bullet4:
+    image = None
+
+    def __init__(self, x=300, y=300):
+        velocity = 1.5
+        damage = 1
+        if Enemy_Bullet4.image == None:
+            Enemy_Bullet4.image = load_image('EnemyGun2.png')
+        self.x, self.y, self.velocity = x, y, velocity
+
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - 12, self.y - 10, self.x + 12, self.y + 10
+
+    def update(self):
+        self.y -= self.velocity
+        self.x -= self.velocity / 3
+
+        if main_state.collide(self, main_state.player):
+            game_world.remove_object(self)
+
+
+        #if main_state.player.hp == 0:
+            #game_world.remove_object(main_state.player)
+            #main_state.player.life -= 1
+
+        if self.y < 20:
+            game_world.remove_object(self)
